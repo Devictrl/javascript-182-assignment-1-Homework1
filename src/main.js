e6fc355089d4d198b8ffb749f4b07f2bb828241a
saba-tyebuchava <81583995+Devictrl@users.noreply.github.com>
2024-11-13 15:13:12 +0400

    saba tyebuchava

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-// Write your code here
+// Write your code her name:saba tyebuchava
+console.log(5 + 3);
+console.log(4 * 3);
+console.log("hello saba");
+console.log("Hi"+ " " + "Saba");
+console.log("hi".toLowerCase());
